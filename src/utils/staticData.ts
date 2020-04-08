27712f8d8e8df6e34d8b3a7c698de20137c4e344
Piotr Bakun <piotr.bakun@hotmail.com>
2020-04-08 23:46:40 +0200

    user details works with api

--- src/utils/staticData.ts
+++ src/utils/staticData.ts
@@ -1,7 +1,7 @@
 
 export const identityToken: string = "token";
-// export const baseUrl = "http://localhost:5000";
-export const baseUrl = "http://apitest.bakson.hostingasp.pl";
+export const baseUrl = "http://localhost:5000";
+// export const baseUrl = "http://apitest.bakson.hostingasp.pl";
 
 export const defaultErrorMessage = "Coś poszło nie tak...";
 export const serverErrorMessage = "Błąd serwera";
@@ -10,3 +10,4 @@ export const saveToFavoriteMessage = "Zapisano do ulubionych!";
 export const deletedFromFavoritesMessage = "Usunięto z ulubionych.";
 export const messageSentToClients = "Wiadomość przekazana pacjentom!";
 export const roomNoChangedMessage = "Numer pokoju został zmieniony!";
+export const userDetailsUpdateSuccess = "Dane użytkownika zostały zaktualizowane";
